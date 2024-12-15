60e0a4dba32f972bb529a6c2c9f398f410f44222
Ishan Vasavada <80118005+ishan654321@users.noreply.github.com>
2024-12-15 02:21:29 -0500

    Update sqlconnect.py

--- sqlconnect.py
+++ sqlconnect.py
@@ -3,7 +3,6 @@ from mysql.connector import Error
 import streamlit as st
 import pandas as pd
 import sqlite3
-from dotenv import load_dotenv
 import toml
 import os
 config = toml.load("config.toml")
